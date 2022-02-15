416aaae5ebb14b47ff49c813a848d5c21c8313f4
ShookLyngs <shook-lyngs@qq.com>
2022-02-15 10:59:37 +0800

    init(project).

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import Clipboard from 'clipboard';
+
+export interface CopyTextOptions extends Clipboard.Options {
+  appendToBody: boolean;
+}
+
+export function copyText(text: string, config: Partial<CopyTextOptions> = {}): Promise<Clipboard.Event> {
+  const options = createOptions(config);
+
+  const button = document.createElement('button');
+  button.style.display = 'none';
+
+  const clipboard = new Clipboard(button, {
+    text: () => text,
+  });
+
+  function destroy() {
+    clipboard.destroy();
+    if (options.appendToBody) {
+      document.body.removeChild(button);
+    }
+  }
+
+  if (options.appendToBody) {
+    document.body.appendChild(button);
+  }
+
+  return new Promise<Clipboard.Event>((resolve, reject) => {
+    clipboard.on('success', (event) => {
+      resolve(event);
+      destroy();
+    });
+    clipboard.on('error', (event) => {
+      reject(event);
+      destroy();
+    });
+
+    button.click();
+  });
+}
+
+function createOptions(options: Partial<CopyTextOptions>): CopyTextOptions {
+  return {
+    appendToBody: true,
+    ...options,
+  };
+}
